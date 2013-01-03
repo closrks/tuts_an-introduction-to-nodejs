e3a156bce56e4c4d94b8d844f17d3e8b24fdf817
Carlos Avila <carlosavila.ce@gmail.com>
2013-01-03 12:58:56 -0800

    change sample.txt to read.txt

--- 3_the-file-system/js/asynchRead.js
+++ 3_the-file-system/js/asynchRead.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 console.log("Starting asynch read...");
-fs.readFile("../data/sample.txt", function (error, data) {
+fs.readFile("../data/read.txt", function (error, data) {
 	console.log("Contents: " + data);
 });
 console.log("Carry on executing...");
--- 3_the-file-system/js/synchRead.js
+++ 3_the-file-system/js/synchRead.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
 console.log("Starting synch read...");
-var data = fs.readFileSync("../data/sample.txt");
+var data = fs.readFileSync("../data/read.txt");
 console.log("Contents: " + data);
 console.log("Carry on executing...");
